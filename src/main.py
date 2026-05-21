27c60466decfa360aa2740c0a2c4693d18085240
Antonio Palacios Hernández <palacioshernandezantonio0489@gmail.com>
2026-05-21 17:15:11 +0200

    version 1.3 para probar la nueva imagen

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ def read_root():
 
 @app.get("/status")
 def status():
-    return {"message": "FastAPI Palacios_Hernández_Antonio v.1.2"}
+    return {"message": "FastAPI Palacios_Hernández_Antonio v.1.3"}
 
 
 @app.get("/db-check")
