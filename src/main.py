1a81933808feadf0ec35363b4ec2dea981df6be0
Antonio Palacios Hernández <palacioshernandezantonio0489@gmail.com>
2026-05-23 12:10:21 +0200

    devops con ubuntu2

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ def read_root():
 
 @app.get("/status")
 def status():
-    return {"message": "FastAPI Palacios_Hernández_Antonio v.1.5"}
+    return {"message": "FastAPI Palacios_Hernández_Antonio v.1.6 con ubuntu"}
 
 
 @app.get("/db-check")
